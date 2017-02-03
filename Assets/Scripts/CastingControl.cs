7b6c800ce91875ad87d65766d386ed6d36385dc4
ShaunKim <shk0307@hotmail.com>
2017-02-02 23:21:39 -0500

    Add few graphics. Clean up spell casting script.

--- Assets/Scripts/CastingControl.cs
+++ Assets/Scripts/CastingControl.cs
@@ -6,8 +6,8 @@ using UnityEngine.VR;
 
 public class CastingControl : MonoBehaviour {
     public bool mouseMode;
-    public GameObject LeftHand;
-    public GameObject RightHand;
+    public GameObject LeftBulletPoint;
+    public GameObject RightBulletPoint;
     public Camera cam;
     public GameObject[] Prefabs;
 
@@ -23,26 +23,28 @@ public class CastingControl : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {    
-        if (ActionControlListener.isRightTriggerFullyPressed() && Time.time > nextSpellLeft)      
-            CastSpell(RightHand, true);        
-        if (ActionControlListener.isLeftTriggerFullyPressed() && Time.time > nextSpellRight)
-            CastSpell(LeftHand, false);
+        if (ActionControlListener.isRightTriggerFullyPressed() && Time.time > nextSpellRight)
+            CastRightHandSpell(RightBulletPoint);
+        if (ActionControlListener.isLeftTriggerFullyPressed() && Time.time > nextSpellLeft)
+            CastLeftHandSpell(LeftBulletPoint);
+    }
+    
+    private void CastLeftHandSpell(GameObject bulletPoint)
+    {
+        nextSpellLeft = Time.time + 0.5f;
+        CastSpell(bulletPoint);
+    }
+
+    private void CastRightHandSpell(GameObject bulletPoint)
+    {
+        nextSpellRight = Time.time + 0.5f;
+        CastSpell(bulletPoint);
     }
 
     // TODO: this needs to take another argument of spell s.t. spell.castRate, spell.delay from spell class
-    private void CastSpell (GameObject hand, bool isLeftHand)
+    private void CastSpell (GameObject bulletPoint)
     {
-        if (isLeftHand) 
-            nextSpellLeft = Time.time + 0.5f;
-        else 
-            nextSpellRight = Time.time + 0.5f;
-
-        Vector3 pos;
-        float yRot = hand.transform.rotation.eulerAngles.y;
-        Vector3 forwardY = Quaternion.Euler(0.0f, yRot, 0.0f) * Vector3.forward;
-        Vector3 forward = hand.transform.forward;
-        Vector3 right = hand.transform.right;
-        Vector3 up = hand.transform.up;
+        Vector3 pos = bulletPoint.transform.position;
         Vector3 rotation = new Vector3(0,0,0);
 
         var spellPrefab = GameObject.Instantiate(Prefabs[0]);
@@ -51,24 +53,21 @@ public class CastingControl : MonoBehaviour {
         if (prefabScript == null)
         {
             // temporary effect, like a fireball
-            prefabScript = spellPrefab.GetComponent<FireBaseScript>();
             if (spellPrefab.GetComponent<FireBaseScript>().IsProjectile)
             {
                 // set the start point near the hand
                 rotation = cam.transform.rotation.eulerAngles;
-                pos = hand.transform.position + forward;
+                Debug.Log("rotation: " + rotation.ToString() + "| pos:" + pos.ToString());
             }
             else
             {
                 prefabScript = spellPrefab.GetComponent<FireConstantBaseScript>();
-                // set the start point in front of the player a ways
-                pos = hand.transform.position + (forwardY * 10.0f);
             }
         }
         else
         {
             // set the start point in front of the player a ways, rotated the same way as the player
-            pos = hand.transform.position + (forwardY * 5.0f);
+            pos = bulletPoint.transform.position;
             rotation = cam.transform.rotation.eulerAngles;
             pos.y = 0.0f;
         }

63871e2f9ab8c3a1c927018d44d43f9e63f182fc
Shaun Kim <shk0307@hotmail.com>
2017-03-12 15:07:55 -0400

    Fix bug where second element resource doesn't get depleted

--- Assets/Scripts/Spell/CastingControl.cs
+++ Assets/Scripts/Spell/CastingControl.cs
@@ -79,8 +79,9 @@ public class CastingControl : MonoBehaviour
         Spells.spellEnum spellEnum = Spells.elementsPairToSpellEnum[elementPair];
         Spells.SpellDetails spellDetail = spellEnumToSpellDetails[spellEnum];
 
+        Elements.elemEnum curSecondElement = elementPair.Second; // this is because "DecrementElement" may modify this
         spellController.DecrementElement(elementPair.First, spellDetail.firstElementCost);
-        spellController.DecrementElement(elementPair.Second, spellDetail.secondElementCost);
+        spellController.DecrementElement(curSecondElement, spellDetail.secondElementCost);
         // === setup done. shoot out bullet
 
         Vector3 pos = bulletPoint.transform.position;

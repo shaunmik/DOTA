255498b9d3afffe8fc89fde3ae6963ea3c108f8d
trallgorm <the_russian_vsn@hotmail.com>
2017-03-14 05:53:47 -0400

    added speed monster

--- Assets/Scripts/MonsterScripts/Ghost/GhostGreen.cs
+++ Assets/Scripts/MonsterScripts/Ghost/GhostGreen.cs
@@ -3,5 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class GhostGreen : Ghost {
-	// TODO: resistances
+    protected override void monsterInit()
+    {
+        MonsterBehavior.FollowStandingTargetStart(agent, target.transform.position);
+    }
+    protected override void monsterMovement()
+    {
+        agent.speed = agent.speed * 1.15f;
+    }
 }
